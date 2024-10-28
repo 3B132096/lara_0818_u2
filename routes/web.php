5b6ba8c0612f02cce9eb41779c2726245a2dde89
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-10-28 17:08:08 +0800

    新增view並回傳3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -8,8 +10,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -17,6 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', function () {
-    return view('home.index');
-});
+//Route::get('home', function () {
+//    return view('home.index');
+//});
+
+Route::get('home', [HomeController::class, 'index']);
